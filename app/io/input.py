584902c912628ae343518cea38a4faa7a96fd4aa
Vladyslava Rudas <byvladyslava@gmail.com>
2024-03-14 17:39:37 +0200

    Added 3 write/read functions to the output.py and input.py files

---
+++ app/io/input.py
@@ -0,0 +1,17 @@
+def input_text_from_console():
+    """
+    To enter text from the console.
+    """
+    pass
+
+def read_from_file(file_path):
+    """
+    To read from a file using built-in python capabilities.
+    """
+    pass
+
+def read_from_file_with_pandas(file_path):
+    """
+    To read from a file using the pandas library.
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,17 @@
+def output_text_from_console():
+    """
+    To output text to the console.
+    """
+    pass
+
+def write_to_file(file_path):
+    """
+    To write to a file using the built-in python features.
+    """
+    pass
+
+def write_to_file_with_pandas(file_path):
+    """
+    To write to a file using the pandas library.
+    """
+    pass
