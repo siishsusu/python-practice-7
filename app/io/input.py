aad77b1e74c13b9849a51400b43e0e8cdc888b29
Vladyslava Rudas <byvladyslava@gmail.com>
2024-03-20 10:04:10 +0200

    Added empty functions for input and output operations in the input.py and output.py files, respectively.

--- app/io/input.py
+++ app/io/input.py
@@ -1,38 +1,12 @@
-import pandas as pd
+
 
 def input_text_from_console():
-    """
-    Function to input text from the console.
-
-    Returns:
-        str: The text entered by the user from the console.
-    """
-    return input("Enter your text: ")
+    pass
 
 
 def read_from_file(file_path):
-    """
-    Function to read text from a file using built-in Python capabilities.
-
-    Args:
-        file_path (str): The path to the file to be read.
-
-    Returns:
-        str: The text read from the file.
-    """
-    with open(file_path, 'r') as file:
-        return file.read()
+    pass
 
 
 def read_from_file_with_pandas(file_path):
-    """
-    Function to read text from a file using the pandas library.
-
-    Args:
-        file_path (str): The path to the file to be read.
-
-    Returns:
-        str: The text read from the file.
-    """
-    data = pd.read_csv(file_path)
-    return data.to_string(index=False)
+    pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,45 +1,11 @@
-import pandas as pd
 
 
 def output_text_to_console(text):
-    """
-    Function to output text to the console.
-
-    This function prompts the user to input text and then prints it to the console.
-
-    Args:
-        text (str): The text to be printed to the console.
-
-    Returns:
-        None
-    """
-    print(text)
+    pass
 
 def write_to_file(file_path, text):
-    """
-    Function to write text to a file using built-in Python capabilities.
-
-    Args:
-        file_path (str): The path to the file to write the text to.
-        text (str): The text to be written to the file.
-
-    Returns:
-        None
-    """
-    with open(file_path, 'w') as file:
-        return file.write(text)
+    pass
 
 
 def write_to_file_with_pandas(file_path, text):
-    """
-    Function to write text to a file using the pandas library.
-
-    Args:
-        file_path (str): The path to the file to write the text to.
-        text (str): The text to be written to the file.
-
-    Returns:
-        None
-    """
-    df = pd.DataFrame([text])
-    df.to_csv(file_path, index=False, header=False)
+    pass
